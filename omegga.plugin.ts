bfb096d9f191e087b4cbc158392a0dd41bd818e5
Dante Stevens <danteorionstevens@gmail.com>
2022-05-17 13:12:56 -0400

    Kill command

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -16,11 +16,26 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
 
   async init() {
     // Write your plugin!
-    this.omegga.on('cmd:test', (speaker: string) => {
-      this.omegga.broadcast(`Hello, ${speaker}!`);
-    });
+    this.omegga.on('cmd:kill',
+    async (speaker: string, target: string, ...args: string[]) => {
+        if(Omegga.getPlayer(speaker).isHost()){
+          const user = this.omegga.getPlayer(speaker);
+          const subject = this.omegga.findPlayerByName(target);
+          if(subject != null){
+            subject.kill();
+            if(args.length > 0){
+              Omegga.whisper(subject, `${user.name} has killed you: ${args.join(' ')}`);
+            } else {
+              Omegga.whisper(subject, `${user.name} has killed you.`);
+            }
+            Omegga.whisper(user, `You have attempted a kill command on <color="ffcc99">${subject.name}.`);
+          } else {
+            Omegga.whisper(user, `Could not find a user with name <color="ffcc99">${target}.`);
+          }
+        }
+      });
 
-    return { registeredCommands: ['test'] };
+    return { registeredCommands: ['kill'] };
   }
 
   async stop() {
